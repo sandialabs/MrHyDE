dcdfb237dc7f02b2f28a34427163c80b470b7757
TimWildey <tmwilde@sandia.gov>
2019-07-03 13:23:13 -0600

    Adding modification to test_functions that uncovered logic error.

--- src/test/test_functions.cpp
+++ src/test/test_functions.cpp
@@ -16,7 +16,7 @@ int main(int argc, char * argv[]) {
   
   Teuchos::RCP<FunctionInterface> functionManager = Teuchos::rcp(new FunctionInterface());
   
-  vector<string> variables = {"a","b","c","d"};
+  vector<string> variables = {"a","b","c","d","p"};
   vector<string> parameters = {"mu"};
   vector<string> disc_parameters = {"ff"};
   
@@ -27,9 +27,9 @@ int main(int argc, char * argv[]) {
   
   vector<basis_RCP> basis = {Teuchos::rcp(new Basis_HGRAD_QUAD_C1_FEM<AssemblyDevice>() )};
   int quadorder = 2;
-  int numElem = 100;
+  int numElem = 10;
   int numip = 4;
-  int numvars = 4;
+  int numvars = 5;
   
   vector<int> cellinfo = {2,numvars,1,0,16,numElem};
   DRV ip, wts, sip, swts;
@@ -47,8 +47,12 @@ int main(int argc, char * argv[]) {
       for (size_t k=0; k<numvars; k++) {
         wkset->local_soln(i,j,k,0) = k+2;
       }
+      for (size_t k=0; k<2; k++) {
+        wkset->ip_KV(i,j,k) = k+2;
+      }
     }
   }
+  KokkosTools::print(wkset->ip);
   
   functionManager->wkset = wkset;
   
@@ -61,12 +65,32 @@ int main(int argc, char * argv[]) {
   string test3 = "8*(pi^2)*sin(2*pi*x+1)*sin(2*pi*y+1)";
   functionManager->addFunction("g",test3,numElem,numip,"ip",0);
   
+  string bubble = "-0.001*(x*x+y*y)";
+  string well = "100.0*exp(bubble)";
+  string welll = "p*well";
+  string wellr = "2.0*well";
+  
+  string source = "wellr - welll";
+//  source = "0.5*p*p+2";
+  functionManager->addFunction("pres","p",numElem,numip,"ip",0);
+  
+  functionManager->addFunction("bubble",bubble,numElem,numip,"ip",0);
+  functionManager->addFunction("well",well,numElem,numip,"ip",0);
+  functionManager->addFunction("welll",welll,numElem,numip,"ip",0);
+  functionManager->addFunction("wellr",wellr,numElem,numip,"ip",0);
+  functionManager->addFunction("source",source,numElem,numip,"ip",0);
+  
   functionManager->validateFunctions();
   functionManager->decomposeFunctions();
   
-  FDATA data1 = functionManager->evaluate("test1","ip",0);
-  FDATA data2 = functionManager->evaluate("test2","ip",0);
-  FDATA data3 = functionManager->evaluate("g","ip",0);
+  functionManager->printFunctions();
+  
+  FDATA datax = functionManager->evaluate("pres","ip",0);
+  
+  FDATA data1 = functionManager->evaluate("bubble","ip",0);
+  FDATA data2 = functionManager->evaluate("well","ip",0);
+  FDATA data3 = functionManager->evaluate("source","ip",0);
+  
   for (size_t i=0; i<numElem; i++) {
     for (size_t j=0; j<numip; j++) {
       cout << "data1(i,j) = " << data1(i,j) << endl;
@@ -79,15 +103,28 @@ int main(int argc, char * argv[]) {
     for (size_t i=0; i<numElem; i++) {
       for (size_t j=0; j<numip; j++) {
         for (size_t k=0; k<numvars; k++) {
-          wkset->local_soln(i,j,k,0) = k+3+m;
+          wkset->local_soln(i,k,j,0) = k+3+m;
         }
       }
     }
+    FDATA datap = functionManager->evaluate("pres","ip",0);
     
-    data1 = functionManager->evaluate("test1","ip",0);
-    data2 = functionManager->evaluate("test2","ip",0);
-    data3 = functionManager->evaluate("g","ip",0);
+    FDATA data1 = functionManager->evaluate("wellr","ip",0);
+    FDATA data2 = functionManager->evaluate("welll","ip",0);
+    FDATA data3 = functionManager->evaluate("source","ip",0);
+    
+    for (size_t i=0; i<numElem; i++) {
+      for (size_t j=0; j<numip; j++) {
+        cout << "datap(i,j) = " << datap(i,j) << endl;
+        cout << "data1(i,j) = " << data1(i,j) << endl;
+        cout << "data2(i,j) = " << data2(i,j) << endl;
+        cout << "data3(i,j) = " << data3(i,j) << endl;
+      }
+    }
+    
   }
+  
+  
   Teuchos::TimeMonitor::summarize();
   
   Kokkos::finalize();
