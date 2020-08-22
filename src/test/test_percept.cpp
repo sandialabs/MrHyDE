9083b5b455ebfa9aeb5f3a37ec54302746d33fe2
TimWildey <tmwilde@sandia.gov>
2020-08-21 18:05:51 -0600

    Added a test to play around with percept for uniform mesh refinement.  Did not actually get it to work due to an MPI error in percept.

---
+++ src/test/test_percept.cpp
@@ -0,0 +1,53 @@
+
+#include "trilinos.hpp"
+#include "preferences.hpp"
+#include "subgridMeshFactory.hpp"
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+  
+  Teuchos::GlobalMPISession mpiSession(&argc, &argv,0);
+  //MpiComm Comm(MPI_COMM_WORLD);
+  Teuchos::RCP<MpiComm> Comm = Teuchos::rcp( new MpiComm(MPI_COMM_WORLD) );
+  
+  Kokkos::initialize();
+
+  {
+    DRV nodes("nodes",4,2);
+    nodes(0,0) = -1.0;
+    nodes(0,1) = -1.0;
+    nodes(1,0) = 1.0;
+    nodes(1,1) = -1.0;
+    nodes(2,0) = 1.0;
+    nodes(2,1) = 1.0;
+    nodes(3,0) = -1.0;
+    nodes(3,1) = 1.0;
+    
+    vector<vector<GO> > connectivity;
+    vector<GO> e0 = {0,1,2,3};
+    
+    string shape = "quad";
+    string blockID = "eb";
+    
+    panzer_stk::SubGridMeshFactory meshFactory(shape, nodes, connectivity, blockID);
+    
+    Teuchos::RCP<panzer_stk::STK_Interface> mesh = meshFactory.buildMesh(MPI_COMM_WORLD);
+    //Teuchos::RCP<panzer_stk::STK_Interface> mesh = meshFactory.buildMesh(Comm);
+    
+    meshFactory.completeMeshConstruction(*mesh,MPI_COMM_WORLD);
+    
+    int numRefine = 2;
+    mesh->refineMesh(numRefine, true);
+    
+  }
+  
+  
+  Kokkos::finalize();
+  
+  
+  int val = 0;
+  return val;
+}
+
+
