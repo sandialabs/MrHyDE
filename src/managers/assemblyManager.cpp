2808970889896946074b57abd23cfe65f2ae909f
Tim Wildey <tmwilde@sandia.gov>
2021-03-06 09:43:58 -0700

    Making the panzer memory purge the default option.
    The DOF manager can always be purged after the setup phase.
    
    The stk interface cannot be purged if visualization is requested.  There should be sufficient safeguards to disable this purge when necessary.

--- src/managers/assemblyManager.cpp
+++ src/managers/assemblyManager.cpp
@@ -1776,7 +1776,9 @@ void AssemblyManager<Node>::scatter(MatType J_kcrs, VecViewType res_view,
 template<class Node>
 void AssemblyManager<Node>::purgeMemory() {
   bool write_solution = settings->sublist("Postprocess").get("write solution",false);
-  if (!write_solution) {
+  bool write_aux_solution = settings->sublist("Postprocess").get("write aux solution",false);
+  bool create_optim_movie = settings->sublist("Postprocess").get("create optimization movie",false);
+  if (!write_solution && !write_aux_solution && !create_optim_movie) {
     mesh.reset();
   }
 }
