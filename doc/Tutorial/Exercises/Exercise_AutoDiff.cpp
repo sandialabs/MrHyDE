6d5df3e9b0f61fbd3f1ece633f30ccc11490bf01
Tim Wildey <tmwilde@sandia.gov>
2021-03-30 10:44:37 -0600

    Adding initial versions of the tutorial exercises.

---
+++ doc/Tutorial/Exercises/Exercise_AutoDiff.cpp
@@ -0,0 +1,52 @@
+
+#include "Sacado.hpp"
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+
+  // We are going to use specific type of AD with a fixed number of derivatives
+  // We only need one derivative for this exercise
+  typedef Sacado::Fad::SFad<double,1> AD;
+
+  // First, let's create an AD variable and see what it is
+  {
+    AD x = 4.2;
+    std::cout << "x = " << x << std::endl;
+    
+    // We can also access the value and derivatives
+    std::cout << "x.val() = " << x.val() << std::endl;
+    std::cout << "x.fastAccessDx(0) = " << x.fastAccessDx(0) << std::endl;
+  }
+  
+  // Note that the derivative is set to 0
+  // Let's create an AD variable that is "seeded"
+  // We will use a different constructor: AD(number of derivs, deriv to seed, value)
+  {
+    AD x(1,0,4.2);
+    std::cout << "x = " << x << std::endl;
+  }
+  
+  // Let's create some more AD variables that depend on x and check the derivatives
+  {
+    AD x(1,0,4.2);
+    AD f = cos(x);
+    std::cout << "f = " << f << std::endl;
+    
+    double true_dfdx = -sin(x.val());
+    std::cout << "Analytical value of df/dx = " << true_dfdx << std::endl;
+    std::cout << "Error in derivative = " << abs(f.fastAccessDx(0) - true_dfdx) << std::endl;
+    
+    AD g = exp(f);
+    std::cout << "g = " << g << std::endl;
+    
+    double true_dgdx = -sin(x.val())*exp(f.val());
+    std::cout << "Analytical value of dg/dx = " << true_dgdx << std::endl;
+    std::cout << "Error in derivative = " << abs(g.fastAccessDx(0) - true_dgdx) << std::endl;
+    
+  }
+  
+  return 0;
+}
+
+
---
+++ doc/Tutorial/Exercises/Exercise_KokkosViews.cpp
@@ -0,0 +1,65 @@
+
+#include "Kokkos_Core.hpp"
+
+// Kokkos doesn't have print options for Views
+// There are very good reasons for this
+// But we want to print things here
+void print(Kokkos::View<double*> V, const std::string & message="") {
+  
+  std::cout << std::endl;
+  std::cout << message << std::endl;
+  std::cout << "Printing data for View: " << V.label() << std::endl;
+  
+  std::cout << "  i  " << "  value  " << std::endl;
+  std::cout << "--------------------" << std::endl;
+  
+  for (long unsigned int i=0; i<V.extent(0); i++) {
+    std::cout << "  " << i << "  " << "  " << "  " << V(i) << "  " << std::endl;
+  }
+  std::cout << "--------------------" << std::endl;
+  
+}
+
+// This is a very basic introduction to using Kokkos::Views
+
+int main(int argc, char * argv[]) {
+
+  Kokkos::initialize();
+
+  {
+    // Let's create 1D array and play with it
+    int N = 5;
+    Kokkos::View<double*> a("a",N);
+    
+    print(a,"Initial values");
+    
+    a(2) = 5.46;
+    print(a,"After modifying an entry");
+    
+    Kokkos::deep_copy(a,1.0);
+    print(a,"After performing deep copy");
+    
+    for (long unsigned int k=0; k<a.extent(0); ++k) {
+      a(k) = 2.0*k;
+    }
+    print(a,"After modifying in a loop");
+    
+    // Let's create another View
+    Kokkos::View<double*> b("b",N);
+    print(b,"Initial values of b");
+    
+    // Setting b equal to a just changes the pointer to the data
+    b = a;
+    print(b,"New values");
+    
+    a(0) = -1.0;
+    print(b,"New values after changing a");
+    
+  }
+  
+  Kokkos::finalize();
+  
+  return 0;
+}
+
+
