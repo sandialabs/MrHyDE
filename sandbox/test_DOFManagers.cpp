d195df295982010e5a53aa7e18b6778eaf505176
Tim Wildey <tmwilde@sandia.gov>
2021-12-12 06:49:52 -0700

    Removed all headers from MrHyDE and cleaned up the test.
    Added a command line option for enabling the periodic BCs.

--- sandbox/test_DOFManagers.cpp
+++ sandbox/test_DOFManagers.cpp
@@ -1,5 +1,16 @@
 
-#include "trilinos.hpp"
+#include "Teuchos_GlobalMPISession.hpp"
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_ParameterList.hpp"
+
+#include "Kokkos_Core.hpp"
+
+#include "Intrepid2_Basis.hpp"
+#include "Intrepid2_HGRAD_HEX_C1_FEM.hpp"
+#include "Intrepid2_HCURL_HEX_I1_FEM.hpp"
+#include "Intrepid2_HDIV_HEX_I1_FEM.hpp"
+#include "Intrepid2_HVOL_C0_FEM.hpp"
+
 #include "Panzer_STK_MeshFactory.hpp"
 #include "Panzer_STK_Interface.hpp"
 #include "Panzer_STK_ExodusReaderFactory.hpp"
@@ -7,23 +18,27 @@
 #include "Panzer_IntrepidFieldPattern.hpp"
 #include "Panzer_DOFManager.hpp"
 
-#include "Intrepid2_HGRAD_HEX_C1_FEM.hpp"
-#include "Intrepid2_HCURL_HEX_I1_FEM.hpp"
-#include "Intrepid2_HDIV_HEX_I1_FEM.hpp"
-#include "Intrepid2_HVOL_C0_FEM.hpp"
-
 using namespace std;
+using Teuchos::RCP;
+using Teuchos::rcp;
 
 int main(int argc, char * argv[]) {
   
+  TEUCHOS_TEST_FOR_EXCEPTION(argc==1,std::runtime_error,"Error: this test requires a mesh file");
+  
   Teuchos::GlobalMPISession mpiSession(&argc, &argv,0);
-  Teuchos::RCP<MpiComm> Comm = Teuchos::rcp( new MpiComm(MPI_COMM_WORLD) );
+  Teuchos::RCP<Teuchos::MpiComm<int>> Comm = Teuchos::rcp( new Teuchos::MpiComm<int>(MPI_COMM_WORLD) );
   
   Kokkos::initialize();
-
+  
+  typedef Teuchos::RCP<const shards::CellTopology> topo_RCP;
+  typedef Teuchos::RCP<Intrepid2::Basis<PHX::Device::execution_space, double, double > > basis_RCP;
+  
   {
     
+    // ==========================================================
     // Create a mesh from the file defined by the user
+    // ==========================================================
     
     std::string input_file_name = argv[1];
     RCP<Teuchos::ParameterList> pl = rcp(new Teuchos::ParameterList);
@@ -31,12 +46,14 @@ int main(int argc, char * argv[]) {
     Teuchos::RCP<panzer_stk::STK_MeshFactory> mesh_factory = Teuchos::rcp(new panzer_stk::STK_ExodusReaderFactory());
     pl->set("File Name",input_file_name);
     
-    RCP<Teuchos::ParameterList> pbc = rcp(new Teuchos::ParameterList);
-    pbc->set("Count",2);
-    pbc->set("Periodic Condition 1","xz-all 1e-12,3D: top;bottom");
-    pbc->set("Periodic Condition 2","yz-all 1e-12,3D: left;right");
-    pl->sublist("Periodic BCs").setParameters( *pbc );
-    
+    // Optional: include periodic BCs
+    if (argc>2) {
+      RCP<Teuchos::ParameterList> pbc = rcp(new Teuchos::ParameterList);
+      pbc->set("Count",2);
+      pbc->set("Periodic Condition 1","xz-all 1e-12,3D: top;bottom");
+      pbc->set("Periodic Condition 2","yz-all 1e-12,3D: left;right");
+      pl->sublist("Periodic BCs").setParameters( *pbc );
+    }
     
     mesh_factory->setParameterList(pl);
     Teuchos::RCP<panzer_stk::STK_Interface> mesh = mesh_factory->buildUncommitedMesh(*(Comm->getRawMpiComm()));
@@ -50,10 +67,16 @@ int main(int argc, char * argv[]) {
     mesh->getElementBlockNames(blocknames);
     int spaceDim = mesh->getDimension();
     
-    // Defined the connectivity manager
+    // ==========================================================
+    // Define the connectivity manager
+    // ==========================================================
+    
     Teuchos::RCP<panzer::ConnManager> conn = Teuchos::rcp(new panzer_stk::STKConnManager(mesh));
     
+    // ==========================================================
     // Test out a few DOF managers
+    // ==========================================================
+    
     {
       if (Comm->getRank() == 0) {
         std::cout << "================================================" << std::endl;
