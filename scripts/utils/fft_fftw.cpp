80d8998d9daccca444e00ec3bbf7e51bcb62d0ed
Bryan Reuter <bwreute@sandia.gov>
2022-02-07 12:04:58 -0700

    Progress on the FFTW interface front. HDF5 now linked and dummy dataset can be written. However, I'm not getting out what I'd expect (WIP).

--- scripts/utils/fft_fftw.cpp
+++ scripts/utils/fft_fftw.cpp
@@ -22,13 +22,15 @@ int main(int argc, char * argv[]) {
  
   // TODO FOO to determine number of histories, load in data, etc.
 
-  int num_histories = 5; // number of time histories owned by the process
+  int num_sensors = 5; // number of time histories owned by the process
 
   int num_sensors_total = 20; // TODO also garbage
 
+  int my_sensors_start = myRank*num_sensors;
+
   int num_snaps = 10; // number of equispaced snapshots in the time domain
   int space_dim = 3; // spatial dimension
-  int num_fields = space_dim*2*num_histories; // E and B fields with space_dim components each
+  int num_fields = space_dim*num_sensors; // E and B fields with space_dim components each
 
   // PHDF5 creation
   // TODO SETUP MPI COMMUNICATOR BETWEEN PROCESSES THAT ACTUALLY HAVE SENSORS
@@ -56,13 +58,15 @@ int main(int argc, char * argv[]) {
   // create the dataspace
 
   hid_t ds_id;
-  hsize_t dims[4] = {num_sensors_total,num_snaps,num_snaps,num_snaps};
-  ds_id = H5Screate_simple(4, dims, // [sensor_id,F_x,F_y,F_z]
+  hsize_t dims[3] = {num_sensors_total,space_dim,num_snaps};
+  ds_id = H5Screate_simple(3, dims, // [sensor_id,spatial_dim,snap]
                            NULL);
 
   // need to create a new hdf5 datatype which matches fftw_complex
-  hsize_t comp_dims[2] = {1,1};
-  hid_t complex_id = H5Tarray_create2(H5T_NATIVE_DOUBLE,2,comp_dims);
+  // TODO not sure about this...
+  // TODO change ??
+  hsize_t comp_dims[1] = {2};
+  hid_t complex_id = H5Tarray_create2(H5T_NATIVE_DOUBLE,1,comp_dims);
   
   // create the B and E frequency domain storage
   hid_t Bfield_id, Efield_id;
@@ -71,25 +75,29 @@ int main(int argc, char * argv[]) {
 
   // set up the portion of the files this process will access
 
-  //hsize_t start[4] = {my_sensor_start,0,0,0};
-  //hsize_t count[4] = {num_sensors,num_snaps,num_snaps,num_snaps};
+  hsize_t start[3] = {my_sensors_start,0,0};
+  hsize_t count[3] = {num_sensors,space_dim,num_snaps};
+
+  err = H5Sselect_hyperslab(ds_id,H5S_SELECT_SET,start,NULL, // contiguous
+                            count,NULL); // contiguous 
 
   // Memory, file dataspace??
-  // TODO HERE
+  // It is my understanding the the file dataspace describes how the data will be laid out in the file
+  // The memory dataspace describes how the data is laid out in memory within the application
 
-  //err = H5Sselect_hyperslab(BLAH,H5S_SELECT_SET,start,NULL, // contiguous
-  //                          count,NULL); // contiguous 
+  // In this example, the FFTW data is a 1D array (of complex) of length
+  // num_snaps*num_fields
+  hsize_t flattened[] = {num_snaps*num_fields};
+  hid_t ms_id = H5Screate_simple(1,flattened,NULL);
 
   // FFTW example
 
-  fftw_complex *myData;
-
+  fftw_complex *myEData,*myBData;
 
   // we will perform ffts of the columns
 
-  myData = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*num_snaps*num_fields);
-
-  //for (unsigned int i=0; i<num_snaps*num_fields; ++i) myData[i][0] = i*1., myData[i][1] = 0.; 
+  myEData = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*num_snaps*num_fields);
+  myBData = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*num_snaps*num_fields);
 
   double x;
   double dx;
@@ -100,7 +108,8 @@ int main(int argc, char * argv[]) {
   for (unsigned int i=0; i<num_snaps; i++) {
     x = i*dx;
     for (unsigned int j=0; j<num_fields; j++) {
-      myData[i*num_fields+j][0] = std::sin(x); myData[i*num_fields+j][1] = 0.;
+      myEData[i*num_fields+j][0] = std::sin(x); myEData[i*num_fields+j][1] = 0.;
+      myBData[i*num_fields+j][0] = std::cos(x); myBData[i*num_fields+j][1] = 0.;
     }
   }
 
@@ -113,24 +122,39 @@ int main(int argc, char * argv[]) {
   int istride = num_fields, ostride = num_fields; // distance between elements in a data line
   int *inembed = n, *onembed = n; // TODO don't fully understand this
 
-  fftw_plan p;
+  fftw_plan Ep,Bp;
 
-  p = fftw_plan_many_dft(rank,n,howmany,myData,inembed,istride,idist,
-                                        myData,onembed,ostride,odist,
-                                        FFTW_FORWARD, FFTW_ESTIMATE); // forward transform
+  Ep = fftw_plan_many_dft(rank,n,howmany,myEData,inembed,istride,idist,
+                                         myEData,onembed,ostride,odist,
+                                         FFTW_FORWARD, FFTW_ESTIMATE); // forward transform
+  Bp = fftw_plan_many_dft(rank,n,howmany,myBData,inembed,istride,idist,
+                                         myBData,onembed,ostride,odist,
+                                         FFTW_FORWARD, FFTW_ESTIMATE); // forward transform
 
-  fftw_execute_dft(p, myData, myData);
+  fftw_execute_dft(Ep, myEData, myEData);
+  fftw_execute_dft(Bp, myBData, myBData);
 
-  if (myRank == 0) {
-    for (unsigned int i=0; i<num_snaps; ++i) {
-      for (unsigned int j=0; j<num_fields; ++j) {
-        std::cout << " i,j :: " << i << ", " << j << " " << myData[i*ostride + j][0] << " + j*" << myData[i*ostride + j][1] << std::endl;
-      }
-    }
-  }
+//  if (myRank == 0) {
+//    for (unsigned int i=0; i<num_snaps; ++i) {
+//      for (unsigned int j=0; j<num_fields; ++j) {
+//        std::cout << " i,j :: " << i << ", " << j << " " << myData[i*ostride + j][0] << " + j*" << myData[i*ostride + j][1] << std::endl;
+//      }
+//    }
+//  }
 
-  fftw_destroy_plan(p);
-  fftw_free(myData);
+  err = H5Dwrite(Efield_id,complex_id,ms_id,ds_id,H5P_DEFAULT,myEData);
+  err = H5Dwrite(Bfield_id,complex_id,ms_id,ds_id,H5P_DEFAULT,myBData);
+
+  err = H5Dclose(Efield_id);
+  err = H5Dclose(Bfield_id);
+
+  H5Sclose(ds_id);
+  H5Fclose(f_id);
+
+  fftw_destroy_plan(Ep);
+  fftw_destroy_plan(Bp);
+  fftw_free(myEData);
+  fftw_free(myBData);
 
   return 0;
 
