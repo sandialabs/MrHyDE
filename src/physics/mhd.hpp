3096a69eb8fc4a51f9dac46ee9ce9010fc6c299a
Daniel Garner Sharp <dgsharp@ecw00704b.sandia.gov>
2023-06-20 09:06:58 -0600

    Starting on MHD physics module

---
+++ src/physics/mhd.hpp
@@ -0,0 +1,80 @@
+/***********************************************************************
+ This is a framework for solving Multi-resolution Hybridized
+ Differential Equations (MrHyDE), an optimized version of
+ Multiscale/Multiphysics Interfaces for Large-scale Optimization (MILO)
+ 
+ Copyright 2018 National Technology & Engineering Solutions of Sandia,
+ LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the
+ U.S. Government retains certain rights in this software.”
+ 
+ Questions? Contact Tim Wildey (tmwilde@sandia.gov) and/or
+ Bart van Bloemen Waanders (bartv@sandia.gov)
+ ************************************************************************/
+
+#ifndef MRHYDE_MHD_H
+#define MRHYDE_MHD_H
+
+#include "physicsBase.hpp"
+
+namespace MrHyDE {
+  
+  /**
+   * \brief mhd physics class.
+   *
+   * This class computes volumetric residuals for the physics described by the following weak form:
+   * \f{eqnarray*}
+   *   \dots
+   * \f}
+   * Where the unknown ___ is the ___.
+   * The following functions may be specified in the input.yaml file: ???
+   */
+  class mhd : public physicsbase {
+  public:
+    
+    // ========================================================================================
+    /* Constructor to set up the problem */
+    // ========================================================================================
+    
+    mhd() {} ;
+    
+    ~mhd() {};
+    
+    mhd(Teuchos::ParameterList & settings, const int & dimension_);
+    
+    // ========================================================================================
+    // ========================================================================================
+    
+    void defineFunctions(Teuchos::ParameterList & fs,
+                         Teuchos::RCP<FunctionManager> & functionManager_);
+    
+    // ========================================================================================
+    // ========================================================================================
+    
+    void volumeResidual();
+        
+    void boundaryResidual();
+    
+    // ========================================================================================
+    // ========================================================================================
+    
+    //void setVars(std::vector<string> & varlist_);
+    
+    void setWorkset(Teuchos::RCP<workset> & wkset_);
+
+  private:
+    
+    int ux_num, uy_num, uz_num, pr_num, Bx_num, By_num, Bz_num, spaceDim;
+    bool useSUPG;
+    
+    Teuchos::RCP<Teuchos::Time> volumeResidualFunc = Teuchos::TimeMonitor::getNewCounter("MrHyDE::mhd::volumeResidual() - function evaluation");
+    Teuchos::RCP<Teuchos::Time> volumeResidualFill = Teuchos::TimeMonitor::getNewCounter("MrHyDE::mhd::volumeResidual() - evaluation of residual");
+    Teuchos::RCP<Teuchos::Time> boundaryResidualFunc = Teuchos::TimeMonitor::getNewCounter("MrHyDE::mhd::boundaryResidual() - function evaluation");
+    Teuchos::RCP<Teuchos::Time> boundaryResidualFill = Teuchos::TimeMonitor::getNewCounter("MrHyDE::mhd::boundaryResidual() - evaluation of residual");
+    Teuchos::RCP<Teuchos::Time> fluxFunc = Teuchos::TimeMonitor::getNewCounter("MrHyDE::mhd::computeFlux() - function evaluation");
+    Teuchos::RCP<Teuchos::Time> fluxFill = Teuchos::TimeMonitor::getNewCounter("MrHyDE::mhd::computeFlux() - evaluation of flux");
+    
+  };
+  
+}
+
+#endif
