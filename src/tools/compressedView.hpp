ca3991041a521fdbe86a71f0cc768fc5e21d1d75
Graham Harper <grahambenharper@gmail.com>
2023-06-27 00:09:48 -0600

    Trying a new approach with the database scaling based on discussions with Tim today

--- src/tools/compressedView.hpp
+++ src/tools/compressedView.hpp
@@ -23,33 +23,67 @@ namespace MrHyDE {
   // New data structure to store compressed views (using a database)
   // =================================================================
   
+  /**
+   * \brief A class which compresses data by storing a view and then allowing random access into it based on a key
+   * Additional functionality allows for on-the-fly scalar multiplication
+  */
   template <class ViewType>
   class CompressedView {
 
   public:
-    ViewType view_;
-    Kokkos::View<LO*,AssemblyDevice> key_;
+    // GH: put booleans at top of class for fastest access
+    //! A boolean whether the key has been allocated. If not, the view behaves like a normal view.
     bool have_key_;
+    //! A boolean whether the scales have been allocated. If not, the view compresses by index only
+    bool have_scales_;
 
+    //! The underlying data
+    ViewType view_;
+    //! The random access key for grabbing the underlying data (used when have_key_ is true)
+    Kokkos::View<LO*,AssemblyDevice> key_;
+    //! The scalar scales for the data (used when have_scales_ is true)
+    View_Sc2 scales_;
+
+    //! Constructor for the case where the view is compressed and scaled.
+    CompressedView(ViewType view, Kokkos::View<LO*,AssemblyDevice> key, View_Sc2 scales)
+    : view_(view),
+      key_(key),
+      scales_(scales)
+    {
+      have_key_ = true;
+      have_scales_ = true;
+    }
+
+    //! Constructor for the case where the view is compressed.
     CompressedView(ViewType view, Kokkos::View<LO*,AssemblyDevice> key)
     : view_(view),
       key_(key)
     {
       have_key_ = true;
+      have_scales_ = false;
+      // TODO: Remove the following lines after performance testing is done
+      have_scales_ = true;
+      scales_ = View_Sc2("database scales", key_.extent(0), 3);
+      Kokkos::deep_copy(scales_,1.0);
     }
 
+    //! Constructor for the case where the view is not compressed.
     CompressedView(ViewType view)
     : view_(view)
     {
       have_key_ = false;
+      have_scales_ = false;
     }
-      
+
+    //! Default constructor
     KOKKOS_INLINE_FUNCTION    
     CompressedView() {};
-    
+
+    //! Default destructor
     KOKKOS_INLINE_FUNCTION
     ~CompressedView() {};
-    
+
+    //! Indexing for 1-dimensional views
     KOKKOS_INLINE_FUNCTION
     typename ViewType::reference_type operator()(const size_type & i0) const {
       if (have_key_) {
@@ -59,7 +93,8 @@ namespace MrHyDE {
         return view_(i0);
       }
     }
-        
+
+    //! Indexing for 2-dimensional views
     KOKKOS_INLINE_FUNCTION
     typename ViewType::reference_type operator()(const size_type & i0, const size_type & i1) const {
       if (have_key_) {
@@ -70,7 +105,8 @@ namespace MrHyDE {
       }
 
     }
-        
+
+    //! Indexing for 3-dimensional views
     KOKKOS_INLINE_FUNCTION
     typename ViewType::reference_type operator()(const size_type & i0, const size_type & i1, const size_type & i2) const {
       if (have_key_) {
@@ -80,17 +116,22 @@ namespace MrHyDE {
         return view_(i0, i1, i2);
       }
     }
-        
+
+    //! Indexing for 4-dimensional views, commonly of the form (elem,dof,pt,dim)
     KOKKOS_INLINE_FUNCTION
-    typename ViewType::reference_type operator()(const size_type & i0, const size_type & i1, const size_type & i2, const size_type & i3) const {
-      if (have_key_) {
+    ScalarT operator()(const size_type & i0, const size_type & i1, const size_type & i2, const size_type & i3) const {
+      if (have_scales_) {
+        return view_(key_(i0), i1, i2, i3)*scales_(i0,i3);
+      }
+      else if (have_key_) {
         return view_(key_(i0), i1, i2, i3);
       }
       else {
         return view_(i0, i1, i2, i3);
       }
     }
-    
+
+    //! The extent of access the compressed view allows. If there is a key, it is the key's extent. Otherwise it is the view's extent.
     KOKKOS_INLINE_FUNCTION
     size_type extent(const size_type & dim) const {
       if (dim==0) {
@@ -105,7 +146,8 @@ namespace MrHyDE {
         return view_.extent(dim);
       }
     }
-    
+
+    //! The size of the compressed view
     KOKKOS_INLINE_FUNCTION
     size_type size() const {
       if (have_key_) {
@@ -116,17 +158,20 @@ namespace MrHyDE {
       }
     }
 
+    //! Get the underlying view
     KOKKOS_INLINE_FUNCTION
     ViewType getView() const {
       return view_;
     }
     
+    //! Print the underlying view and key
     void print() {
       std::cout << "Printing CompressedView" << std::endl;
       KokkosTools::print(view_);
       KokkosTools::print(key_);
     }
 
+    //! Print the memory savings for the compressed view
     void printMemory() {
       std::cout << "Printing memory savings for CompressedView" << std::endl;
       std::cout << "Key length = " << key_.extent(0) << std::endl;
@@ -141,4 +186,3 @@ namespace MrHyDE {
 }
 
 #endif
-
