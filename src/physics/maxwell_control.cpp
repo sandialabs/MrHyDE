feed248f394797bd9d0c01a139b273131bb4bf66
Yaw Owusu-Agyemang <yawwusugh@gmail.com>
2025-09-30 14:04:37 -0400

    Examples for Maxwell's proceedings paper (#5)
    * Added changes and examples for the Summer 2025 maxwells proceedings paper
    
    * Further instructions on how to run examples
    
    * Addressed Denis' review comments
    
    * Added 2D jou file and preprocessing instructions
    
    * Final changes

--- src/physics/maxwell_control.cpp
+++ src/physics/maxwell_control.cpp
@@ -17,17 +17,39 @@ maxwell_control<EvalT>::maxwell_control(Teuchos::ParameterList & settings, const
   label = "maxwell_control";
   
   spaceDim = dimension_;
-  myvars.push_back("E");
-  myvars.push_back("B");
-  
-  mybasistypes.push_back("HCURL");
-  if (spaceDim == 2) {
-    mybasistypes.push_back("HVOL");
+  include_Beqn = false;
+  include_Eeqn = false;
+    
+  if (settings.isParameter("active variables")) {
+    string active = settings.get<string>("active variables");
+    std::size_t foundE = active.find("E");
+    if (foundE!=std::string::npos) {
+      include_Eeqn = true;
+    }
+    std::size_t foundB = active.find("B");
+    if (foundB!=std::string::npos) {
+      include_Beqn = true;
+    }
   }
-  else if (spaceDim == 3) {
-    mybasistypes.push_back("HDIV");
+  else {
+    include_Beqn = true;
+    include_Eeqn = true;
   }
   
+  if (include_Eeqn) {
+    myvars.push_back("E");
+    mybasistypes.push_back("HCURL");
+  }
+  if (include_Beqn) {
+    myvars.push_back("B");
+    if (spaceDim == 2) {
+      mybasistypes.push_back("HVOL");
+    }
+    else if (spaceDim == 3) {
+      mybasistypes.push_back("HDIV");
+    }
+  }
+    
   useLeapFrog = settings.get<bool>("use leap frog",false);
 }
 
@@ -36,13 +58,16 @@ maxwell_control<EvalT>::maxwell_control(Teuchos::ParameterList & settings, const
 
 template<class EvalT>
 void maxwell_control<EvalT>::defineFunctions(Teuchos::ParameterList & fs,
-                                             Teuchos::RCP<FunctionManager<EvalT> > & functionManager_) {
+                              Teuchos::RCP<FunctionManager<EvalT> > & functionManager_) {
   
   functionManager = functionManager_;
   
   functionManager->addFunction("current x",fs.get<string>("current x","0.0"),"ip");
   functionManager->addFunction("current y",fs.get<string>("current y","0.0"),"ip");
   functionManager->addFunction("current z",fs.get<string>("current z","0.0"),"ip");
+  functionManager->addFunction("control current x",fs.get<string>("control current x","0.0"),"ip");
+  functionManager->addFunction("control current y",fs.get<string>("control current y","0.0"),"ip");
+  functionManager->addFunction("control current z",fs.get<string>("control current z","0.0"),"ip");
   functionManager->addFunction("mu",fs.get<string>("permeability","1.0"),"ip");
   functionManager->addFunction("refractive index",fs.get<string>("refractive index","1.0"),"ip");
   functionManager->addFunction("epsilon",fs.get<string>("permittivity","1.0"),"ip");
@@ -56,31 +81,13 @@ void maxwell_control<EvalT>::defineFunctions(Teuchos::ParameterList & fs,
 template<class EvalT>
 void maxwell_control<EvalT>::volumeResidual() {
   
-  
-  int E_basis = wkset->usebasis[Enum];
-  int B_basis = wkset->usebasis[Bnum];
-  
-  Vista<EvalT> mu, epsilon, sigma, rindex;
-  Vista<EvalT> current_x, current_y, current_z;
-  
-  {
-    Teuchos::TimeMonitor funceval(*volumeResidualFunc);
-    current_x = functionManager->evaluate("current x","ip");
-    current_y = functionManager->evaluate("current y","ip");
-    if (spaceDim > 2) {
-      current_z = functionManager->evaluate("current z","ip");
-    }
-    mu = functionManager->evaluate("mu","ip");
-    epsilon = functionManager->evaluate("epsilon","ip");
-    rindex = functionManager->evaluate("refractive index","ip");
-    sigma = functionManager->evaluate("sigma","ip");
-  }
-  
   Teuchos::TimeMonitor resideval(*volumeResidualFill);
   
   int stage = wkset->current_stage;
   
-  {
+  if (include_Beqn) {
+    int B_basis = wkset->usebasis[Bnum];
+    
     if (spaceDim == 2) {
       // (dB/dt + curl E,V) = 0
       
@@ -119,7 +126,8 @@ void maxwell_control<EvalT>::volumeResidual() {
         }
       }
       else {
-        auto curlE = wkset->getSolutionField("curl(E)[x]");
+        
+        auto curlE = wkset->getSolutionField("curl(E)");
         parallel_for("Maxwells B volume resid",
                      RangePolicy<AssemblyExec>(0,wkset->numElem),
                      KOKKOS_LAMBDA (const int elem ) {
@@ -130,6 +138,7 @@ void maxwell_control<EvalT>::volumeResidual() {
             }
           }
         });
+        
       }
     }
     else if (spaceDim == 3) {
@@ -166,6 +175,7 @@ void maxwell_control<EvalT>::volumeResidual() {
           });
         }
         else {
+          
           parallel_for("Maxwells B volume resid",
                        RangePolicy<AssemblyExec>(0,wkset->numElem),
                        KOKKOS_LAMBDA (const int elem ) {
@@ -180,6 +190,7 @@ void maxwell_control<EvalT>::volumeResidual() {
               }
             }
           });
+          
         }
       }
       else {
@@ -201,14 +212,38 @@ void maxwell_control<EvalT>::volumeResidual() {
             }
           }
         });
+        
       }
     }
   }
   
-  {
-    // (eps*dE/dt,V) - (1/mu B, curl V) + (sigma E,V) = -(current,V)
-    // Rewritten as: (eps*dEdt + sigma E + current, V) - (1/mu B, curl V) = 0
+  if (include_Eeqn) {
+    // (eps*dE/dt,V) - (1/mu B, curl V) + (sigma E,V) = -(current, V) - (ctrl_current,V)
+    // Rewritten as: (eps*dEdt + sigma E + current, V) - (1/mu B - ctrl_current, curl V) = 0
     
+    int E_basis = wkset->usebasis[Enum];
+    
+    Vista<EvalT> mu, epsilon, sigma, rindex;
+    Vista<EvalT> current_x, current_y, current_z;
+    Vista<EvalT> ctrl_current_x, ctrl_current_y, ctrl_current_z;
+    
+    {
+      Teuchos::TimeMonitor funceval(*volumeResidualFunc);
+      current_x = functionManager->evaluate("current x","ip");
+      current_y = functionManager->evaluate("current y","ip");
+      ctrl_current_x = functionManager->evaluate("control current x","ip");
+      if (spaceDim > 2) {
+        current_z = functionManager->evaluate("current z","ip");
+        ctrl_current_y = functionManager->evaluate("control current y","ip");
+        ctrl_current_z = functionManager->evaluate("control current z","ip");
+      }
+      mu = functionManager->evaluate("mu","ip");
+      epsilon = functionManager->evaluate("epsilon","ip");
+      rindex = functionManager->evaluate("refractive index","ip");
+      sigma = functionManager->evaluate("sigma","ip");
+    }
+
+    // Note: Here B is a scalar field and so is ctrl_current    
     if (spaceDim == 2) {
       if (!useLeapFrog || stage == 1) {
         auto basis = wkset->basis[E_basis];
@@ -229,7 +264,7 @@ void maxwell_control<EvalT>::volumeResidual() {
           for (size_type pt=0; pt<basis.extent(2); pt++ ) {
             EvalT f0 = (epsilon(elem,pt)*rindex(elem,pt)*rindex(elem,pt)*dEx_dt(elem,pt) + (sigma(elem,pt)*Ex(elem,pt) + current_x(elem,pt)))*wts(elem,pt);
             EvalT f1 = (epsilon(elem,pt)*rindex(elem,pt)*rindex(elem,pt)*dEy_dt(elem,pt) + (sigma(elem,pt)*Ey(elem,pt) + current_y(elem,pt)))*wts(elem,pt);
-            EvalT c0 = -1.0/mu(elem,pt)*B(elem,pt)*wts(elem,pt);
+            EvalT c0 = (ctrl_current_x(elem,pt) - 1.0/mu(elem,pt)*B(elem,pt))*wts(elem,pt);
             for (size_type dof=0; dof<basis.extent(1); dof++ ) {
               res(elem,off(dof)) += f0*basis(elem,dof,pt,0) + c0*basis_curl(elem,dof,pt,0) + f1*basis(elem,dof,pt,1);
             }
@@ -238,6 +273,8 @@ void maxwell_control<EvalT>::volumeResidual() {
       }
     }
     else if (spaceDim == 3) {
+      // (eps*dE/dt,V) - (1/mu B, curl V) + (sigma E,V) = -(current + ctrl_current,V)
+      // Rewritten as: (dEdt + (1/eps)*sigma E + (1/eps)*current, V) - (1/eps)*(1/mu B + (1/eps)*ctrl_current, curl V) = 0
       
       if (!useLeapFrog || stage == 1) {
         auto basis = wkset->basis[E_basis];
@@ -264,9 +301,9 @@ void maxwell_control<EvalT>::volumeResidual() {
             EvalT f1 = (rindex(elem,pt)*rindex(elem,pt)*dEy_dt(elem,pt) + 1.0/eps*(sigma(elem,pt)*Ey(elem,pt) + current_y(elem,pt)))*wts(elem,pt);
             EvalT f2 = (rindex(elem,pt)*rindex(elem,pt)*dEz_dt(elem,pt) + 1.0/eps*(sigma(elem,pt)*Ez(elem,pt) + current_z(elem,pt)))*wts(elem,pt);
             
-            EvalT c0 = -1.0/mu(elem,pt)*1.0/eps*Bx(elem,pt)*wts(elem,pt);
-            EvalT c1 = -1.0/mu(elem,pt)*1.0/eps*By(elem,pt)*wts(elem,pt);
-            EvalT c2 = -1.0/mu(elem,pt)*1.0/eps*Bz(elem,pt)*wts(elem,pt);
+            EvalT c0 = 1.0/eps*(ctrl_current_x(elem,pt) - 1.0/mu(elem,pt)*Bx(elem,pt))*wts(elem,pt);
+            EvalT c1 = 1.0/eps*(ctrl_current_y(elem,pt) - 1.0/mu(elem,pt)*By(elem,pt))*wts(elem,pt);
+            EvalT c2 = 1.0/eps*(ctrl_current_z(elem,pt) - 1.0/mu(elem,pt)*Bz(elem,pt))*wts(elem,pt);
             for (size_type dof=0; dof<basis.extent(1); dof++ ) {
               res(elem,off(dof)) += f0*basis(elem,dof,pt,0) + c0*basis_curl(elem,dof,pt,0);
               res(elem,off(dof)) += f1*basis(elem,dof,pt,1) + c1*basis_curl(elem,dof,pt,1);
